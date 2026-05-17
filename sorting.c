f622e14fbddf35cf13e013afb6812c8257ed5fd2
Rustam Juraev <rustam.juraev03@gmail.com>
2026-05-17 21:56:51 +0500

    Bubble sort algorithm from scratch

---
+++ sorting.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *bubble_sort(int array[], int n)
+{
+    int *ptr = array;
+    printf("This is the value of the first element in the array %d\n",*ptr);
+    if (ptr == NULL)
+    {
+        printf("Array is empty, no values in it \n");
+        return NULL;
+    }
+
+    for (int i=0; i<n; i++)
+    {
+        ptr = &array[0];
+        for(int z=n-1; z>0; z--)
+        {
+            if( *ptr > *(ptr+1))
+            {
+                int temp;
+                temp = *(ptr);
+                *(ptr) = *(ptr+1);
+                *(ptr+1) = temp;
+            }
+            ptr++;
+        }
+    }
+    return array;
+
+
+}
+
+void display(int *ptr)
+{
+    printf("Started printing the sorted array \n");
+    for(int i=0; i<10; i++)
+    {
+        printf("%d --> ",*(ptr+i));
+    }
+}
+
+int main(void)
+{
+    int arr1[] = {42, 7, 19, 73, 4, 88, 15, 63, 29, 50};
+    int *ptr = arr1;
+    ptr = bubble_sort(arr1,10);
+    display(ptr);
+    return 0;
+}
